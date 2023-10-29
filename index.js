629a0c74b1fc67ad1cf5d3da0d4e0fa5f142dde4
shaheerkhan111 <149266345+shaheerkhan111@users.noreply.github.com>
2023-10-29 14:04:13 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+// class school {
+//     name;
+//     students = [];
+//     tudents = [];
+//     addstudent(stdobj) {
+//         this.students.push(stdobj);
+//     }
+//     constructor(name) {
+//         this.name = name;
+//     }
+//     addteeacher(tecobj) {
+//         this.students.push(tecobj);
+//     }
+// }
+// class student {
+//     name;
+//     age;
+//     schoolname;
+//     constructor(name, age, schoolname) {
+//         this.name = name;
+//         this.age = age;
+//         this.schoolname = schoolname;
+//     }
+// }
+// class teacher {
+//     name;
+//     age;
+//     schoolname;
+//     email;
+//     contact;
+//     constructor(name, age, schoolname, email, contact) {
+//         this.name = name;
+//         this.age = age;
+//         this.schoolname = schoolname;
+//         this.email = email;
+//         this.contact = contact;
+//     }
+// }
+// let school1 = new school("Alpha");
+// let school2 = new school("CIPS");
+// let s1 = new student("Rayyan", 40, school1.name);
+// let s2 = new student("subhan", 10, school2.name);
+// let s3 = new student("ayan", 15, school2.name);
+// let t1 = new teacher("Amir", 25, school1.name, "abc@gmail.com", 101210);
+// let t2 = new teacher("Danish", 36, school2.name, "ijk@gmail.com", 123641);
+// let t3 = new teacher("Shaheer", 24, school2.name, "hdg@gmail.com", 654654);
+// school1.addstudent(s1);
+// school2.addstudent(s2);
+// school2.addstudent(s3);
+// school1.addteeacher(t1);
+// school2.addteeacher(t2);
+// school2.addteeacher(t3);
+// console.log(t1);
+// console.log(t2);
+// console.log(t3);
+// console.log(school1);
+// console.log(school2);
+// export {};
---
+++ index.ts
@@ -0,0 +1,79 @@
+// import inquirer from "inquirer";
+// import chalk from "chalk";
+
+// class school {
+//     name:string
+//     students:any = [];
+//     tudents:any = [];
+//     addstudent(stdobj:any){
+//         this.students.push(stdobj)
+//     }
+//     constructor(name:string){
+//         this.name = name;
+//     }
+//     addteeacher(tecobj:any){
+//         this.students.push(tecobj)
+//     }
+   
+// }
+// class student {
+
+// name :string
+// age:number
+// schoolname:string
+
+// constructor(name :string,age:number,schoolname:string){
+//     this.name =name
+//     this.age =age
+//     this.schoolname = schoolname
+
+// }}
+
+// class teacher {
+//     name :string
+//     age:number
+//     schoolname:string
+//     email:string
+//     contact:number
+    
+//     constructor(name :string,age:number,schoolname:string,  email:string, contact:number){
+//         this.name =name
+//         this.age =age
+//         this.schoolname = schoolname
+//          this.email=email
+//         this.contact=contact}}
+ 
+    
+
+
+// let school1 = new school("Alpha")
+// let school2 = new school("CIPS")
+
+
+// let s1 = new student("Rayyan", 40 ,school1.name)
+// let s2 = new student("subhan", 10 ,school2.name)
+// let s3 = new student("ayan", 15 ,school2.name)
+
+// let t1 = new teacher("Amir", 25 ,school1.name,"abc@gmail.com",101210)
+// let t2 = new teacher("Danish", 36,school2.name,"ijk@gmail.com",123641)
+// let t3 = new teacher("Shaheer", 24 ,school2.name,"hdg@gmail.com",654654)
+
+// school1.addstudent(s1)
+// school2.addstudent(s2)
+// school2.addstudent(s3)
+
+
+// school1.addteeacher(t1)
+// school2.addteeacher(t2)
+// school2.addteeacher(t3)
+
+// console.log(t1)
+// console.log(t2)
+// console.log(t3)
+
+
+
+
+
+// console.log(school1)
+// console.log(school2)
